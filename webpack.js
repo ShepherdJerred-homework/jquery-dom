9359d31981226d995f77e41f39182f8f4ae434c6
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-04 21:58:32 -0500

    Strip comments

--- webpack.js
+++ webpack.js
@@ -2,30 +2,17 @@ const webpack = require('webpack');
 const uglifyjsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-
-  // All input files are found in the lib/client directory
   context: __dirname + "/lib/client",
-
-  // Entry points: pack these files with all their dependencies
   entry: {
-    jquery: "jquery",                          // third party library
-    main: "./main.js",                         // client file (relative to context)
-    multi: ["./one.js", "two.js", "three.js"]  // multiple client files
+    main: "./main.js",
   },
-
-  // Output specification - one output per entry
   output: {
-    filename: "[name].js",            // use entry name as the filename
-    path: __dirname + "/static/lib"   // store in static directory
+    filename: "[name].js",
+    path: __dirname + "/static/lib"
   },
-
-  // Enable sourcemaps for debugging webpack's output
   devtool: "source-map",
-
-  // Modules
   module: {
     rules: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
       {
         test: /\.js$/,
         exclude: /node_modules/,
@@ -34,10 +21,7 @@ module.exports = {
       }
     ]
   },
-
-  // Plug-ins
   plugins: [
-    // Any modules in jquery should be removed from other files
     new webpack.optimize.CommonsChunkPlugin({
       name: "jquery",
       minChunks: Infinity,
